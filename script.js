3c7bd4ef6dbce472dac521f0ecd66c4c45805df3
ziaritarik <ziaritarik.u@gmail.com>
2021-09-29 22:00:50 +0200

    hhhh

--- script.js
+++ script.js
@@ -1,30 +1,43 @@
+//-----------------Les Objets--------------------
+
+/*let object={
+    propertyNam1:propertyVlue1,
+    propertyNam1:propertyVlue1,
+    method1:fuction(){
+
+        }
+        };*/
 
 
-function fname(x,y){
-   let Result=x+y;
-    console.log(Result);
-    return Result;
+        //methode 1
+        let dog={
+            name:"chaly",
+            color:"blue",
+            age:5
+                };
+        //methode 2
+        let personne = new Object();
+        personne.name="tarik";
+        personne.prenom="ziari";
+        personne.age=26;
+        personne.aboie=function(num){
+            while(num>0){
+                console.log(num.toString() +"  Wouf ");
+                num--;
+            }
+           
+        }
+        personne.aboie(4);
+
+
+      //  console.log(dog.name)
+      //  console.log(dog.color)
+      //  console.log(dog.age)
+
+for(let property in dog){
+    console.log(dog[property])
 }
-console.log(fname(10,12));
 
-
-
-
-let a =5;
-let b = 6;
-
-function multiply(number1,number2){
-    return number1*number2;
+for(let property in personne){
+    console.log(personne[property])
 }
-
-var result = multiply(a,b);
-console.log(result);
-
-function multi(num1,num2,num3){
-    return num1*num2*num3;
-}
-
-let result1 = multi(a,b,a);
-console.log(result1);
-
-
