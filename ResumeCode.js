7a15dfc30deb87c728b8abb3680b826e9e160159
ziaritarik <ziaritarik.u@gmail.com>
2021-09-29 22:27:54 +0200

    Function constructeur

--- ResumeCode.js
+++ ResumeCode.js
@@ -243,3 +243,30 @@ for(let property in personne){
 }
 
 
+
+
+
+        //Function Constructeur
+
+
+        function Dog(name,color,age){
+            this.name=name;
+            this.color=color;
+            this.age=age;
+            this.aboie=function(num){
+                while(num>0){
+                    console.log(num.toString() +"  Wouf "+this.name);
+                    num--;
+                }
+            }
+               
+     }
+        
+     let petitcaniche= new Dog("Choupette","white",4);
+     let grosPitbull= new Dog("Rex","black",2);
+     console.log(petitcaniche);
+     console.log(grosPitbull);
+     petitcaniche.aboie(1);
+     grosPitbull.aboie(1);
+
+
--- script.js
+++ script.js
@@ -1,43 +1,22 @@
-//-----------------Les Objets--------------------
-
-/*let object={
-    propertyNam1:propertyVlue1,
-    propertyNam1:propertyVlue1,
-    method1:fuction(){
-
-        }
-        };*/
+        //Function Constructeur
 
 
-        //methode 1
-        let dog={
-            name:"chaly",
-            color:"blue",
-            age:5
-                };
-        //methode 2
-        let personne = new Object();
-        personne.name="tarik";
-        personne.prenom="ziari";
-        personne.age=26;
-        personne.aboie=function(num){
-            while(num>0){
-                console.log(num.toString() +"  Wouf ");
-                num--;
+     function Dog(name,color,age){
+            this.name=name;
+            this.color=color;
+            this.age=age;
+            this.aboie=function(num){
+                while(num>0){
+                    console.log(num.toString() +"  Wouf "+this.name);
+                    num--;
+                }
             }
-           
-        }
-        personne.aboie(4);
-
-
-      //  console.log(dog.name)
-      //  console.log(dog.color)
-      //  console.log(dog.age)
-
-for(let property in dog){
-    console.log(dog[property])
-}
-
-for(let property in personne){
-    console.log(personne[property])
-}
+               
+     }
+        
+     let petitcaniche= new Dog("Choupette","white",4);
+     let grosPitbull= new Dog("Rex","black",2);
+     console.log(petitcaniche);
+     console.log(grosPitbull);
+     petitcaniche.aboie(1);
+     grosPitbull.aboie(1);
